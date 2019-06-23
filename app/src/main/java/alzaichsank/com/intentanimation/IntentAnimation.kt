57f5fdf0d39d5630a4858ddf01814e1ca0e4c5b9
Alza Devloper <alzaichsank@gmail.com>
2019-06-23 19:08:01 +0700

    Intent constructor animation

---
+++ app/src/main/java/alzaichsank/com/intentanimation/IntentAnimation.kt
@@ -0,0 +1,41 @@
+package alzaichsank.com.intentanimation
+
+import android.app.Activity
+import android.content.Context
+
+/**
+ * Created by alzaichsank on 2019-06-23.
+ */
+
+class AcTrans private constructor(private var builder: Builder) {
+
+    class Builder(private var context: Context) {
+
+        val activity = this.context as Activity
+
+        fun performNoAnimation() {
+            activity.overridePendingTransition(0, 0)
+        }
+
+        fun performSlideToLeft() {
+            activity.overridePendingTransition(R.anim.slide_left_start, R.anim.slide_left_end)
+        }
+
+        fun performSlideToRight() {
+            activity.overridePendingTransition(R.anim.slide_right_start, R.anim.slide_right_end)
+        }
+
+        fun performSlideToBottom() {
+            activity.overridePendingTransition(R.anim.slide_bottom_start, R.anim.slide_bottom_end)
+        }
+
+        fun performSlideToTop() {
+            activity.overridePendingTransition(R.anim.slide_top_start, R.anim.slide_top_end)
+        }
+
+        fun performFade() {
+            activity.overridePendingTransition(R.anim.fade_start, R.anim.fade_end)
+        }
+    }
+
+}
