657dac59db6cb8f715d580a6c73cb8d80b6b666b
Alza Devloper <alzaichsank@gmail.com>
2019-08-12 18:16:27 +0700

    Add example layout and logic

--- exampleapp/src/main/java/alzaichsank/com/exampleapp/MainActivity.kt
+++ exampleapp/src/main/java/alzaichsank/com/exampleapp/MainActivity.kt
@@ -1,5 +1,12 @@
 package alzaichsank.com.exampleapp
 
+import alzaichsank.com.exampleapp.MenuActivity.Companion.explode
+import alzaichsank.com.exampleapp.MenuActivity.Companion.fade
+import alzaichsank.com.exampleapp.MenuActivity.Companion.normal
+import alzaichsank.com.exampleapp.MenuActivity.Companion.slide1
+import alzaichsank.com.exampleapp.MenuActivity.Companion.slide2
+import alzaichsank.com.exampleapp.MenuActivity.Companion.slide3
+import alzaichsank.com.exampleapp.MenuActivity.Companion.slide4
 import alzaichsank.com.intentanimation.AnimIntent
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -11,9 +18,48 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button_main.setOnClickListener {
-            startActivity(Intent(this,MenuActivity::class.java))
+        intentAction()
+
+    }
+
+    private fun intentAction() {
+        val go = Intent(this, MenuActivity::class.java)
+        button_main_slide.setOnClickListener {
+            go.putExtra(MenuActivity.TYPE_INTENT, normal)
+            this.startActivity(go)
+            AnimIntent.Builder(this).performNoAnimation()
+        }
+        button_main_slide1.setOnClickListener {
+            go.putExtra(MenuActivity.TYPE_INTENT, slide1)
+            this.startActivity(go)
             AnimIntent.Builder(this).performSlideToLeft()
         }
+        button_main_slide2.setOnClickListener {
+            go.putExtra(MenuActivity.TYPE_INTENT, slide2)
+            this.startActivity(go)
+            AnimIntent.Builder(this).performSlideToRight()
+        }
+        button_main_slide3.setOnClickListener {
+            go.putExtra(MenuActivity.TYPE_INTENT, slide3)
+            this.startActivity(go)
+            AnimIntent.Builder(this).performSlideToTop()
+        }
+        button_main_slide4.setOnClickListener {
+            go.putExtra(MenuActivity.TYPE_INTENT, slide4)
+            this.startActivity(go)
+            AnimIntent.Builder(this).performSlideToBottom()
+        }
+        button_main_fade.setOnClickListener {
+            go.putExtra(MenuActivity.TYPE_INTENT, fade)
+            this.startActivity(go)
+            AnimIntent.Builder(this).performFade()
+        }
+
+        button_main_explode.setOnClickListener {
+            go.putExtra(MenuActivity.TYPE_INTENT, explode)
+            this.startActivity(go)
+            AnimIntent.Builder(this).performExplode()
+        }
+
     }
 }
--- exampleapp/src/main/java/alzaichsank/com/exampleapp/MenuActivity.kt
+++ exampleapp/src/main/java/alzaichsank/com/exampleapp/MenuActivity.kt
@@ -1,18 +1,55 @@
 package alzaichsank.com.exampleapp
 
 import alzaichsank.com.intentanimation.AnimIntent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
+    companion object {
+        const val TYPE_INTENT = "type.intent"
+        const val normal = "normal"
+        const val slide1 = "slide1"
+        const val slide2 = "slide2"
+        const val slide3 = "slide3"
+        const val slide4 = "slide4"
+        const val fade = "fade"
+        const val explode = "explode"
+    }
+
+    private var intentString = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        intentString = intent.getStringExtra(TYPE_INTENT)
+        text_view_result.text = "this intent $intentString"
     }
 
     override fun onBackPressed() {
         super.onBackPressed()
-        AnimIntent.Builder(this).performSlideToRight()
+        when (intentString) {
+            normal -> {
+                AnimIntent.Builder(this).performNoAnimation()
+            }
+            slide1 -> {
+                AnimIntent.Builder(this).performSlideToRight()
+            }
+            slide2 -> {
+                AnimIntent.Builder(this).performSlideToLeft()
+            }
+            slide3 -> {
+                AnimIntent.Builder(this).performSlideToBottom()
+            }
+            slide4 -> {
+                AnimIntent.Builder(this).performSlideToTop()
+            }
+            fade -> {
+                AnimIntent.Builder(this).performFade()
+            }
+            explode -> {
+                AnimIntent.Builder(this).performExplode()
+            }
+        }
     }
 }
